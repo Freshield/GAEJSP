88290ed9d912b7e8df2fd57bf2e85a6e78db72b7
Freshield <zxdsw199182@gmail.com>
2016-03-22 15:22:30 -0400

    done jsp

--- real-headfirst/beerV1/src/com/example/web/BeerSelect.java
+++ real-headfirst/beerV1/src/com/example/web/BeerSelect.java
@@ -1,21 +1,31 @@
 package com.example.web;
 
 import javax.servlet.*;
-
 import javax.servlet.http.*;
 
 import java.io.*;
+import java.util.*;
+
+import com.example.model.*;
 
 public class BeerSelect extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request,
 			HttpServletResponse response)
 			throws IOException, ServletException{
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("Beer Selection Advice<br>");
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		//out.println("Beer Selection Advice<br>");
 		String c = request.getParameter("color");
-		out.println("<br>Got beer color "+c);
+		//out.println("<br>Got beer color "+c);
+		
+
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(c);
+
+		request.setAttribute("styles", result);
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+		view.forward(request, response);
 	}
 
 }
