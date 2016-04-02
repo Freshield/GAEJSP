dca9a486f3ff0607c8b2c017b7dfaad7ad04629c
Freshield <zxdsw199182@gmail.com>
2016-04-02 16:35:18 -0400

    test classic tag

---
+++ real-headfirst/Chp10.2/src/foo/Classic1.java
@@ -0,0 +1,22 @@
+package foo;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+import java.io.*;
+
+public class Classic1 extends TagSupport{
+	
+	public int doStartTag() throws JspException{
+		
+		JspWriter out = pageContext.getOut();
+		
+		try{
+			out.println("classic tag output");
+		}catch(IOException ex){
+			throw new JspException("IOException-"+ex.toString());
+		}
+		return SKIP_BODY;
+	}
+
+}
